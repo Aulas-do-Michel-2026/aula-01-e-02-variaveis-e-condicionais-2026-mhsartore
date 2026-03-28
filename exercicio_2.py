49b82f1b74c5772365c9f61edf862d6eb08fadd6
Matteo Sartore <matteohstr@gmail.com>
2026-03-28 18:09:59 -0300

    Update exercicio_2.py
    Adicionada a resolução do exercício 2.

--- exercicio_2.py
+++ exercicio_2.py
@@ -13,3 +13,8 @@ Digite o peso do paciente (em kg):
 Resposta:
 Média: 140 mg
 """
+
+peso_kg = int(input("Insira seu peso (kg):"))
+dose_medicamento_mg = peso_kg * 2
+
+print(f"A dose de medicamento indicada para {peso_kg}kg é de {dose_medicamento_mg}mg.")
