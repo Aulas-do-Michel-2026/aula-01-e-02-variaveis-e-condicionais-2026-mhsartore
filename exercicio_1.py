5b38158da6aaa1c0285d05774cb3400cdb37f597
Matteo Sartore <matteohstr@gmail.com>
2026-03-28 18:04:03 -0300

    Update exercicio_1.py
    Resolução exercício 1 adicionada

--- exercicio_1.py
+++ exercicio_1.py
@@ -19,3 +19,11 @@ Dica: Use inputs para receber os dados!
 Lembre de converter ele para o tipo necessário!
 Print na tela com "print"
 """
+
+nota_1 = float(input("Insira a primeira nota:"))
+nota_2 = float(input("Insira a segunda nota:"))
+nota_3 = float(input("Insira a terceira nota:"))
+
+media_notas = round(((nota_1 + nota_2 + nota_3)/3), 2)
+
+print(f"A média aritmética das notas {nota_1}, {nota_2}, {nota_3} é {media_notas}")
