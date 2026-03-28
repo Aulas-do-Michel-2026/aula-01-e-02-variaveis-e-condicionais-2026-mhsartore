e3d6aaed280ded56f46d74695cc857beeba9f438
Matteo Sartore <matteohstr@gmail.com>
2026-03-28 20:44:52 -0300

    Update exercicio_7.py
    Adicionada resolução do exercício 7

--- exercicio_7.py
+++ exercicio_7.py
@@ -88,3 +88,20 @@ Esse exercício está com uma resolução no notebook da aula.
 
 Tente, se não der, olhe lá!
 """
+
+freq_populacional = int(input("Frequência Populacional:"))
+gene = str(input("Gene da Variante:"))
+impacto = str(input("Impacto (ALTO ou BAIXO):"))
+reads = int(input("Quantidade de Reads:"))
+vaf = int(input("Frequência Alélica:"))
+
+genes_excecao = (gene == "HFE") or (gene == "MEFV") or (gene == "GJB2")
+
+if reads < 10 or vaf < 20:
+    print("Não é relevante, provável artefato.")
+elif impacto == "ALTO":
+    print("A variante é relevante.")
+elif freq_populacional > 5 and not genes_excecao:
+    print("Não é relevante.")
+else:
+    print("A variante é relevante.")
